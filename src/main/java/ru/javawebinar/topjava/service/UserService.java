d57e77f6a0b6494296a3fbc29a4d323dc6bbfc5e
Vladislav Malinnikov <vladmalff@gmail.com>
2023-10-30 16:03:02 +0300

    applied app layers

--- src/main/java/ru/javawebinar/topjava/service/UserService.java
+++ src/main/java/ru/javawebinar/topjava/service/UserService.java
@@ -1,6 +1,5 @@
 package ru.javawebinar.topjava.service;
 
-import org.springframework.stereotype.Service;
 import ru.javawebinar.topjava.model.User;
 import ru.javawebinar.topjava.repository.UserRepository;
 
@@ -9,14 +8,9 @@ import java.util.List;
 import static ru.javawebinar.topjava.util.ValidationUtil.checkNotFound;
 import static ru.javawebinar.topjava.util.ValidationUtil.checkNotFoundWithId;
 
-@Service
 public class UserService {
 
-    private final UserRepository repository;
-
-    public UserService(UserRepository repository) {
-        this.repository = repository;
-    }
+    private UserRepository repository;
 
     public User create(User user) {
         return repository.save(user);
